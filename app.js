695226ad6546d39c8df7702c3a5ef7843a3c4645
Muhammadnoman004 <143292643+Muhammadnoman004@users.noreply.github.com>
2023-10-18 18:07:14 +0500

    Second commit

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-let getDevNews = document.getElementById("DivNews")
-let apiKey =  "8c8d1dfc665a40c5affe11ea36632f31"
-let apiUrl = "https://newsapi.org/v2/everything?from=2023-09-16&sortBy=publishedAt&q=" 
-let input = "Pakistan"
+let getDevNews = document.getElementById("DivNews");
+let apiKey = "8c8d1dfc665a40c5affe11ea36632f31";
+let apiUrl =
+  "https://newsapi.org/v2/everything?from=2023-09-30&sortBy=publishedAt&q=";
+let input = "Pakistan";
 
 fetch(apiUrl + input + `&apiKey=${apiKey}`)
-.then(Response => Response.json())
-.then(data => {
-    console.log(data.articles[0])
-    let urlImage = data.articles[0].urlToImage
-    let news = data.articles[0].title
-    let des = data.articles[0].description
-    // console.log(urlImage)
-    // console.log(news)
-    // console.log(des)
-    for(let i = 0; i < data.length; i++){
-
-    }
-   
+  .then((Response) => Response.json())
+  .then((data) => {
+    let arr = data.articles;
 
 
-})
+    arr.forEach((currNews) => {
+      let urlImage = currNews.urlToImage;
+      let news = currNews.title;
+      let des = currNews.description;
+
+      if (urlImage != null) {
+        getDevNews.innerHTML += `<div class="card m-4" style="width: 18rem;">
+            <img src="${urlImage}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${news}</h5>
+              <p class="card-text">${des}</p>
+            </div>
+          </div>`;
+      } else {
+        return;
+      }
+    });
+  });
