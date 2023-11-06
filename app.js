0328ba4ab6eac8213268ca693e67339faddca910
Muhammadnoman004 <143292643+Muhammadnoman004@users.noreply.github.com>
2023-11-06 16:44:29 +0500

    done

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 let getDevNews = document.getElementById("DivNews");
+let getInp = document.getElementById("inp");
+let getBtn = document.getElementById("search")
 let apiKey = "8c8d1dfc665a40c5affe11ea36632f31";
 let apiUrl =
-  "https://newsapi.org/v2/everything?from=2023-09-30&sortBy=publishedAt&q=";
-let input = "Pakistan";
-
-fetch(apiUrl + input + `&apiKey=${apiKey}`)
-  .then((Response) => Response.json())
-  .then((data) => {
-    let arr = data.articles;
-
-
-    arr.forEach((currNews) => {
-      let urlImage = currNews.urlToImage;
-      let news = currNews.title;
-      let des = currNews.description;
-
-      if (urlImage != null) {
-        getDevNews.innerHTML += `<div class="card m-4" style="width: 18rem;">
-            <img src="${urlImage}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${news}</h5>
-              <p class="card-text">${des}</p>
-            </div>
-          </div>`;
-      } else {
-        return;
-      }
+  `https://newsapi.org/v2/everything?from=2023-10-6&sortBy=publishedAt&q=`;
+  
+  
+  getBtn.addEventListener("click" , function news(){
+  let input = `${getInp.value}`
+  getDevNews.innerHTML = "";
+  fetch(apiUrl + input + `&apiKey=${apiKey}`)
+    .then((Response) => Response.json())
+    .then((data) => {
+      let arr = data.articles;
+  
+      arr.forEach((currNews) => {
+        let urlImage = currNews.urlToImage;
+        let news = currNews.title;
+        let des = currNews.description;
+  
+        if (urlImage != null) {
+          getDevNews.innerHTML += `<div class="card m-4" style="width: 18rem;">
+              <img src="${urlImage}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${news}</h5>
+                <p class="card-text">${des}</p>
+              </div>
+            </div>`;
+        } else {
+          return;
+        }
+      });
     });
-  });
+
+})
